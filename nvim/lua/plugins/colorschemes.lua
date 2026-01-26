d5d8ecf433c40909f23b61ca873811a29ff6ec58
andrew nguyen <andrew.nguyen1@techvify.com.vn>
2026-01-26 16:49:05 +0700

    fixup

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -3,67 +3,68 @@ local transparent = true
 local add = MiniDeps.add
 
 if colorscheme == "gruvbox" then
-	add("ellisonleao/gruvbox.nvim")
-	require("gruvbox").setup({
-		contrast = "",
-		transparent_mode = transparent,
-	})
+  add("ellisonleao/gruvbox.nvim")
+  require("gruvbox").setup({
+    contrast = "",
+    transparent_mode = transparent,
+  })
 end
 if colorscheme == "gruvbox-material" then
-	add("f4z3r/gruvbox-material.nvim")
-	require("gruvbox-material").setup({
-		background = {
-			transparent = transparent,
-		},
-	})
+  add("f4z3r/gruvbox-material.nvim")
+  require("gruvbox-material").setup({
+    background = {
+      transparent = transparent,
+    },
+  })
 end
 if colorscheme == "kanagawa" then
-	add("rebelot/kanagawa.nvim")
-	require("kanagawa").setup({
-		theme = "dragon",
-		dimInactive = true,
-	})
+  add("rebelot/kanagawa.nvim")
+  require("kanagawa").setup({
+    theme = "dragon",
+    dimInactive = true,
+  })
 end
 if colorscheme == "catppuccin" then
-	add({
-		source = "catppuccin/nvim",
-		name = "catppuccin",
-	})
-	require("catppuccin").setup({
-		dim_inactive = {
-			enabled = true,
-		},
-		transparent_background = false,
-		auto_integrations = true,
-		integrations = {
-			mini = {
-				enabled = true,
-				indentscope_color = "rosewater",
-			},
-		},
-	})
+  add({
+    source = "catppuccin/nvim",
+    name = "catppuccin",
+  })
+  require("catppuccin").setup({
+    dim_inactive = {
+      enabled = true,
+    },
+    transparent_background = false,
+    auto_integrations = true,
+    integrations = {
+      mini = {
+        enabled = true,
+        indentscope_color = "rosewater",
+      },
+    },
+  })
 end
 if colorscheme == "tokyonight" then
-	add("folke/tokyonight.nvim")
-	require("tokyonight").setup({
-		style = "night",
-	})
+  add("folke/tokyonight.nvim")
+  require("tokyonight").setup({
+    style = "night",
+  })
 end
 
 if colorscheme == "rose-pine" then
-	add({
-		source = "rose-pine/neovim",
-		name = "rose-pine",
-	})
-	require("rose-pine").setup({
-		styles = {
-			transparency = transparent,
-		},
-	})
+  add({
+    source = "rose-pine/neovim",
+    name = "rose-pine",
+  })
+  require("rose-pine").setup({
+    dark_variant = "moon",
+    styles = {
+      transparency = transparent,
+    },
+  })
 end
 
 if colorscheme == "mini" then
-	vim.cmd.colorscheme("miniwinter")
+  vim.cmd.colorscheme("miniwinter")
 else
-	vim.cmd.colorscheme(colorscheme)
+  vim.cmd.colorscheme(colorscheme)
 end
