913442dc5000580dd1587cf9bd5d5c51928a3837
andrew nguyen <ngphuchoanganh@gmail.com>
2026-07-19 14:09:47 +0700

    fix(powershell): fix yazi preview

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,8 @@
 # --- Environment ---
 $env:XDG_DATA_HOME       = "$env:USERPROFILE\.local\share"
 $env:XDG_CONFIG_HOME     = "$env:USERPROFILE\.config"
-$env:YAZI_CONFiG_HOME    = "$env:USERPROFILE\.config\yazi"
+$env:YAZI_CONFIG_HOME    = "$env:USERPROFILE\.config\yazi"
+$env:YAZI_FILE_ONE       = Join-Path (Get-Item (Get-Command git.exe).Path).Directory.Parent.FullName "apps\git\current\usr\bin\file.exe"
 $env:BAT_CONFIG_PATH     = "$env:USERPROFILE\.config\bat\bat.conf"
 $env:BAT_CONFIG_DIR      = "$env:USERPROFILE\.config\bat"
 $env:EDITOR              = "nvim"
