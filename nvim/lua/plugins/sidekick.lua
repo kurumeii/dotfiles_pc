a22f252ae5dab809140aac39101856c6c85f1060
andrew nguyen <ngphuchoanganh@gmail.com>
2026-09-16 10:47:18 +0700

    feat(nvim): add mimo

--- nvim/lua/plugins/sidekick.lua
+++ nvim/lua/plugins/sidekick.lua
@@ -14,6 +14,7 @@ local agent_tools = {}
 for name in pairs(require("sidekick.config").cli.tools) do
   agent_tools[name] = { cmd = resolve_cmd(name) }
 end
+agent_tools.mimo = { cmd = resolve_cmd("mimo") }
 
 -- AGENT
 require("sidekick").setup({
